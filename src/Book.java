16b27c65d97ef23570aaecd1d33c4c0060409550
Aida1108 <mi.aida@yandex.ru>
2022-12-26 19:49:38 +0500

    Сделано ДЗ

---
+++ src/Book.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Book {
+    //Задание 1
+    private String title;
+    private Author author;
+    private int year;
+
+    public Book(String title, Author author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public Author getAuthor(){
+        return author;
+    }
+    public void setAuthor(Author author){
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+    public void setYear(int year){
+        this.year = year;
+    }
+    //Задание 2
+    public String toString() {
+        return "Название книги " + title + " Автор " + author + " Год " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && title.equals(book.title) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("Задание 1");
+        Author author1 = new Author("Александр", "Пушкин");
+        Author author2 = new Author("Антон", "Чехов");
+        System.out.println("Имя " + author1.getName() + " Фамилия " + author1.getLastName());
+        System.out.println("Имя " + author2.getName() + " Фамилия " + author2.getLastName());
+
+        Book book1 = new Book("Капитанская дочка", author1, 1835);
+        Book book2 = new Book("Вишневый сад", author2, 1903);
+        System.out.println( "Название книги " + book1.getTitle() + " Автор " + author1.getName() + " " + author2.getLastName() + " Год " + book1.getYear() );
+        System.out.println( "Название книги " + book2.getTitle() + " Автор " + author2.getName() + " " + author2.getLastName() + " Год " + book2.getYear() );
+        book1.setYear(1836);
+        System.out.println( "Название книги " + book1.getTitle() + " Автор " + author1.getName() + " " + author2.getLastName() + " Год " + book1.getYear() );
+
+        //Задание 2
+        System.out.println("Задание 2");
+        System.out.println(author1);
+        System.out.println(author2);
+        System.out.println(author1.equals(author2));
+        System.out.println(book1);
+        System.out.println(book2);
+        System.out.println(book1.equals(book2));
+
+
     }
 }
